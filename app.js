d531144e3e76438cf2e2093ff45e0bfb0a65e276
blazgocompany <96586789+blazgocompany@users.noreply.github.com>
2024-08-09 10:10:59 -0700

    Added enhanced loggging for app.js

--- app.js
+++ app.js
@@ -67,6 +67,11 @@ app.get("/somepage", async (req, res) => {
   });
 
   try {
+    console.log(process.env)
+    console.log(process.env.DB_HOST)
+    console.log(process.env.DB_USER)
+    console.log(process.env.DB_PASSWORD)
+    console.log(process.env.DB_NAME)
     console.log("Connecting to database...");
     await client.connect();
     console.log("Connected to database.");
